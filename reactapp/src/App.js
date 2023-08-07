d5082f5c75458b74337f7c1381370833d12468e9
Iamneo <techadmin@iamneo.ai>
2023-08-07 08:33:37 +0000

    Backup Commit

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { Banner } from './components/UI/Banner/Banner';
 import Button from './components/UI/Button/Button';
 import './components/UI/Card/Card';
-import './components/Quizstyle.css'
+import './components/UI/Quizstyle.css';
 
 const App = () => {
   const Questionbank = [
