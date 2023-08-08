0a31d03dd3c778d68533aca1fe5b06419e61d1ad
Iamneo <techadmin@iamneo.ai>
2023-08-08 06:25:30 +0000

    Backup Commit

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -3,6 +3,7 @@ import Banner from "./components/UI/Banner/Banner";
 import Card from "./components/UI/Card/Card";
 import Button from "./components/UI/Button/Button";
 import './Quiz.css';
+
 const App = () => {
   const [questions, setQuestions] = useState([
     {
@@ -11,39 +12,49 @@ const App = () => {
       correctOption: 2,
       selectedOption: null,
     },
-    {
-      question: 'Which famous scientist developed the theory of relativity?',
-      options: ['Isaac Newton', 'Albert Einstein', 'Galileo Galilei', 'Nikola Tesla'],
-      correctOption: 1,
-      selectedOption: null,
-    },
-    {
-      question: 'Which gas do plants use for photosynthesis?',
-      options: ['Carbon Dioxide', 'Oxygen', 'Nitrogen', 'Hydrogen'],
-      correctOption: 0,
-      selectedOption: null,
-    },
-    {
-      question: 'Which planet is known as the Red Planet?',
-      options: ['Venus', 'Mars', 'Jupiter', 'Mercury'],
-      correctOption: 1,
-      selectedOption: null,
-    },
+    // ... (other questions)
   ]);
+
   const [currentIndex, setCurrentIndex] = useState(0);
   const [showResults, setShowResults] = useState(false);
   const [score, setScore] = useState(0);
 
-  const handleAttempt = (isCorrect) => {
+  const handleAttempt = (selectedOptionIndex) => {
+    const currentQuestion = questions[currentIndex];
+    const isCorrect = selectedOptionIndex === currentQuestion.correctOption;
+  
+    const updatedQuestions = questions.map((question, index) => {
+      if (index === currentIndex) {
+        return {
+          ...question,
+          selectedOption: selectedOptionIndex,
+        };
+      }
+      return question;
+    });
+  
+    setQuestions(updatedQuestions);
+  
     if (isCorrect) {
       setScore(score + 1);
     }
-    const updatedQuestions = [...questions];
-    updatedQuestions[currentIndex].selectedOption = updatedQuestions[currentIndex].correctOption;
-    setQuestions(updatedQuestions);
-    setCurrentIndex(currentIndex + 1);
+  
     if (currentIndex === questions.length - 1) {
       setShowResults(true);
+    } else {
+      setCurrentIndex(currentIndex + 1);
+    }
+  };
+  
+
+    const updatedQuestions = [...questions];
+    updatedQuestions[currentIndex].selectedOption = selectedOptionIndex;
+    setQuestions(updatedQuestions);
+
+    if (currentIndex === questions.length - 1) {
+      setShowResults(true);
+    } else {
+      setCurrentIndex(currentIndex + 1);
     }
   };
 
@@ -65,7 +76,7 @@ const App = () => {
       {showResults ? (
         <div className="results">
           <h2>Your Score: {score}</h2>
-          <Button onClick={handleRestart}>Start Quiz</Button>
+          <Button onClick={handleRestart}>Restart Quiz</Button>
         </div>
       ) : (
         <div>
@@ -73,7 +84,6 @@ const App = () => {
             question={questions[currentIndex].question}
             options={questions[currentIndex].options}
             selectedOption={questions[currentIndex].selectedOption}
-            correctOption={questions[currentIndex].correctOption}
             attempt={handleAttempt}
           />
           <Button
